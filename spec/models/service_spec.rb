bf0e367f7572de1f76e68cd5599b70efd0bfd422
RyanDBarnett <conner2468@gmail.com>
2021-03-08 13:44:58 -0700

    Add service create test

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Service, type: :model do
+  it 'can be created' do
+    service = Service.create
+    expect(service).to be_an_instance_of(Service)
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_numericality_of(:cost) }
