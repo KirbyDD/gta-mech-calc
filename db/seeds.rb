5119ef781e1b3964b340bd80698284e248f20705
DeMarcus Kirby <tkd024@gmail.com>
2021-03-12 00:08:08 -0700

    Add seeds for services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Service.destroy_all
+Service.create(name: "Engine", cost: 11000)
+Service.create(name: "Max Brakes", cost: 3100)
+Service.create(name: "Transmission", cost: 12500)
+Service.create(name: "Suspension", cost: 2700)
+Service.create(name: "Armor", cost: 15000)
+Service.create(name: "Turbo", cost: 14000)
+Service.create(name: "Rims", cost: 1500)
+Service.create(name: "Tint", cost: 1750)
+Service.create(name: "Xenons", cost: 2125)
+Service.create(name: "Rim Color", cost: 1200)
+Service.create(name: "Blue Package", cost: 1200)
+Service.create(name: "Neon Kit", cost: 1200)
+Service.create(name: "Neon Color", cost: 1200)
