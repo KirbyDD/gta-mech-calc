47b08915ffad005e3a8a45a612c83d45a7840549
RyanDBarnett <conner2468@gmail.com>
2021-03-15 18:59:21 -0600

    Change service total cost model test to rely on seeded services instead of creating them

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -14,11 +14,7 @@ describe Service, type: :model do
   describe 'class methods' do
     describe 'total_cost' do
       it 'returns the cost of all services' do
-        service1 = Service.create(name: "Engine", cost: 11000)
-        service2 = Service.create(name: "Max Brakes", cost: 3100)
-        expected = service1.cost + service2.cost
-
-        expect(Service.total_cost).to eq(expected)
+        expect(Service.total_cost).to eq(68475.0)
       end
     end
   end
